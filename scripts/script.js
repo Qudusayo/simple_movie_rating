da515c73e2ce7032ceaac588c8ff468539ad7168
Qudusayo <zeekimono@gmail.com>
2020-10-24 11:46:09 +0100

    Simple fix trick

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,49 @@
 // Components
 const addMovie = document.querySelector("#addMovie");
+
 const title = document.querySelector("#title");
 const rating = document.querySelector("#rating");
 
+const tableContent = document.querySelector('.table-content');
+
 // Variables
-const movies = [
-    {
-        title: "Mark Otto",
-        rating: 4,
-    },
-    {
-        title: "Jacob Thornton",
-        rating: 7,
-    },
-    {
-        title: "Larry the Bird",
-        rating: 2,
-    },
-];
+var movies = [];
+let x = 0;
+
+// Functions
+function syncMovie(){
+    const movieRate = (title, rate, id) =>`
+    <tr>
+        <td class="text-center">${title}</td>
+        <td class="text-center">${rate}</td>
+        <td class="text-right">
+            <button class="btn btn-danger" onClick="removeId(${id})">Delete</button>
+        </td>
+    </tr>
+    `
+    tableContent.innerHTML = ''
+    movies.forEach(movie => {
+        tableContent.innerHTML += movieRate(movie.title, movie.rating, movie.id);
+    })
+}
+
+function removeId(id){
+    const remains = movies.filter((movie) => movie.id !== id);
+    movies = remains;
+    syncMovie()
+}
 
 // EventListeners
 addMovie.addEventListener("click", (e) => {
     e.preventDefault();
-    movies.push({
+    x++
+
+    movies.unshift({
         title: title.value,
         rating: rating.value,
+        id: x
     });
-    console.log(movies);
-});
 
-// Functions
+    syncMovie();
+    title.value = rating.value = ''
+});
