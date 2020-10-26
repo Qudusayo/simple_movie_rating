edf04f1d6fb0c0a886925f25a600b65d4d11076f
Onyibe Godstime <godstimeonyibe2@gmail.com>
2020-10-26 13:01:00 +0100

    Adding more functionality to the js file

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,60 +4,44 @@ const title = document.querySelector("#title");
 const rating = document.querySelector("#rating");
 const tableContent = document.querySelector('.table-content');
 
+// Variables
+let movies = [], id = 0, remove
 
-let saveMovie = [], count = 0
-let movies = localStorage.getItem('Movie')
-
-addMovie.addEventListener('submit', e => {
-  e.preventDefault()
-  addMovies()
-})
-
-let displayMovie = () => {
-  if (saveMovie.length <= 0) {
-    alert('Empty list')
-  } else {
-    alert('movie list')
-    const movieRate = (id,title, rate, remove) =>`
+// Functions
+let loadMovies = () => {
+    const movieRating = (id,title, rate, deleteId) =>`
     <tr>
         <td class="text-center">${id}</td>
         <td class="text-center">${title}</td>
         <td class="text-center">${rate}</td>
         <td class="text-right">
-            <button class="btn btn-danger" onClick="removeId(${remove})">Delete</button>
+            <button class="btn btn-danger" onClick="removeId(${deleteId})">Delete</button>
         </td>
     </tr>
     `
     tableContent.innerHTML = ''
     movies.forEach(movie => {
-        tableContent.innerHTML += addMovies(movie.id,movie.title, movie.rating, movie.remove);
+        tableContent.innerHTML += movieRating(movie.id,movie.title, movie.rating, movie.remove);
     })
-  }
 }
-// if (movies) {
-//   saveMovie = JSON.parse(movies)
-//   id = saveMovie.length
-//   syncMovies(saveMovie)
-// } else {
-//   saveMovie = []
-//   id = 0
-// }
 
-// let syncMovies = (array) =>{
-//   array.forEach( movie => {
-//     addMovies(movie.id, movie.title, movie.rate, movie.delete)
-//   })
-// }
+let removeId = (remove) => {
+    const remains = movies.filter((movie) => movie.remove !== remove);
+    movies = remains;
+    loadMovies()
+}
 
-// let addMovies = (id,name, rate, remove) => {
-//   if (remove) return;
-//     const movie = `
-//     <tr>
-//         <td class="text-center">${id}</td>
-//         <td class="text-center">${name}</td>
-//         <td class="text-center">${rate}</td>
-//         <td class="text-right">
-//             <button class="btn btn-danger" onClick="removeId(${remove})">Delete</button>
-//         </td>
-//     </tr> `
-// }
+// event listener that submit the movies to the table
+addMovie.addEventListener("submit", e => {
+    e.preventDefault();
+    id++
+    movies.push({
+        id: id,
+        title: title.value,
+        rating: rating.value,
+        // remove: remove.value
+    });
+
+    loadMovies();
+    title.value = rating.value = ''
+});
